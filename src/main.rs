88480052d374e20b114b01fae3f47f90d67a2974
Muhammad Waqar <MW6069605@GMAIL.COM>
2023-03-10 12:34:52 +0500

    --

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,14 @@
-fn main() {
-    // let a = 10;
-    // let b= 5;
-    // println!("Hello, world!,{} {}",a,b);
+fn main(){
+    let arr=[0,2,1,3];
+//derefrancing array
+    let slice = &arr[1..3]; //[1,2]
 
-    //unsinged integer
-    //u8,u16,u32,u64,u128
-    // let unsigned: u8=100;
+    borrowing_slice(arr, slice);
+}
 
-    // //signed interger
-    // //i8,i16,i32,i64,i128
-    // let signed:i8=-10;
-
-    // let float: f32=1.2;
-
-    // println!("unsigned: {} signed: {}, float: {}",unsigned,signed,float);
-
-    // //char con only be
-    // let letter="CHARACTER01";
-    // let emoji="\u{1F600}";
-    // println!("Letter : {}, Emoji: {}",letter,emoji);
-
-    // let is_true:bool= true;
-    // println!("IsTrue: {}",is_true);
-
-//     let arr:[u8;3]=[1,2,3];
-//     let other_arr:[u8;5]=[100,2,2,2,2];
-
-//     println!("Index: {},length: {}",arr[0],other_arr.len());
-
-// //print structure of the array and other objects
-//     println!("{:?}",other_arr);
-
-let tuple:(u8,bool,f32)=(5,true,2.1);
-let tuple2=(3,5);
-//index of tuple
-println!("First {}, Second {} , Third {}",tuple.0,tuple.1,tuple.2);
-
-println!("{:?}",tuple2);
-
-let (a,b,c)=tuple;
-
-//destructuring 
-println!("first {}, second {}, third {}",a,b,c);
+fn borrowing_slice(arr:[u8;4],slice: &[u8]){
+    println!("{:?}",arr);
+    println!("{:?}",slice);
+    println!("length:{}",slice.len());
+    println!("{} {}",slice[0],slice[1]);
 }
---
+++ src/test.rs
@@ -0,0 +1,55 @@
+fn main() {
+    // let a = 10;
+    // let b= 5;
+    // println!("Hello, world!,{} {}",a,b);
+
+    //unsinged integer
+    //u8,u16,u32,u64,u128
+    // let unsigned: u8=100;
+
+    // //signed interger
+    // //i8,i16,i32,i64,i128
+    // let signed:i8=-10;
+
+    // let float: f32=1.2;
+
+    // println!("unsigned: {} signed: {}, float: {}",unsigned,signed,float);
+
+    // //char con only be
+    // let letter="CHARACTER01";
+    // let emoji="\u{1F600}";
+    // println!("Letter : {}, Emoji: {}",letter,emoji);
+
+    // let is_true:bool= true;
+    // println!("IsTrue: {}",is_true);
+
+//     let arr:[u8;3]=[1,2,3];
+//     let other_arr:[u8;5]=[100,2,2,2,2];
+
+//     println!("Index: {},length: {}",arr[0],other_arr.len());
+
+// //print structure of the array and other objects
+//     println!("{:?}",other_arr);
+
+// let tuple:(u8,bool,f32)=(5,true,2.1);
+// let tuple2=(3,5);
+// //index of tuple
+// println!("First {}, Second {} , Third {}",tuple.0,tuple.1,tuple.2);
+
+// println!("{:?}",tuple2);
+
+// let (a,b,c)=tuple;
+
+// //destructuring 
+// println!("first {}, second {}, third {}",a,b,c);
+// println!("{}",is_even(3));
+
+// let mut number=5;
+// number=6;
+// println!("{}",number);
+}
+//public must be defined else the functiom will be private
+// pub fn is_even(num:u8)->bool{
+//     let digit:u8=num%2;
+//     digit==0 //return bool
+// }
