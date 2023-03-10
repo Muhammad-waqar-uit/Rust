d5f9dba8781ec078a1257fce692d9a6a37fe5729
Muhammad Waqar <MW6069605@GMAIL.COM>
2023-03-10 14:22:18 +0500

    --

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,41 @@ fn main(){
 
 //     borrowing_slice(arr, slice);
 
-    let str: &str = "Hello World!";
-    let mut string: String = String::from("HelloWorld");
-    let slice = &string[..6];
-    println!("{} {} {}", str, string, slice);
+    // let str: &str = "Hello World!";
+    // let mut string: String = String::from("HelloWorld");
+    // let slice = &string[..6];
+    // println!("{} {} {}", str, string, slice);
 
-    string.push('a');
-    let new_slice = &string[..];
-    println!("{} {} {}", str, string, new_slice);
+    // string.push('a');
+    // let new_slice = &string[..];
+    // println!("{} {} {}", str, string, new_slice);
 
-    let local =string.replace("Hello","Check");
-    println!("{} {} {}", str, local, new_slice);
+    // let local =string.replace("Hello","Check");
+    // println!("{} {} {}", str, local, new_slice);
+
+
+    // let n=-1;
+    // if n>0{
+    //     println!("Greater Than zero");
+    // }else if n<0{
+    //     println!("Less than zero");
+    // }else{
+    //     println!("Is zero")
+    // }
+
+    //for loop in rust
+    // for i in 0..6{
+    //     println!("{}",i);
+    // }
+     let mut i=0;
+    while i<4{
+        println!("{}",i);
+        i+=1;
+        if i==3 {
+            print!("Exit");
+            break;
+        }
+    }
 }
 
 
